00412a4e4649ac24197365cd93a2f7285ac5f612
Júlio César <julio@MacBook-Pro-de-Julio.local>
2016-02-08 09:59:59 -0200

    handlebars, loading page, style

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var gulp      = require('gulp');
-var gutil     = require('gulp-util');
-var stylus    = require('gulp-stylus');
-var nib       = require('nib');
-var jeet      = require('jeet');
-var uglify    = require('gulp-uglify');
-var plumber   = require('gulp-plumber');
-var watch     = require('gulp-watch');
+var gulp        = require('gulp');
+var gutil       = require('gulp-util');
+var stylus      = require('gulp-stylus');
+var nib         = require('nib');
+var jeet        = require('jeet');
+var uglify      = require('gulp-uglify');
+var plumber     = require('gulp-plumber');
+var watch       = require('gulp-watch');
+var browserSync = require('browser-sync').create();
 
 //compass error
 var onError = function (err) {  
@@ -13,40 +14,47 @@ var onError = function (err) {
   console.log(err);
 };
 
+// Static Server + watching stylus/html files
+gulp.task('serve', ['stylus'], function() {
+
+    browserSync.init({
+       proxy: "localhost:1337/weather"
+    });
+
+    gulp.watch("src/styl/**/*.styl", ['stylus']);
+    gulp.watch("src/js/*.js", ['scripts']);
+    gulp.watch("src/img/**/*.png", ['img']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+
+});
+
+gulp.task('default', ['serve']);
+
 //minificando js
 gulp.task('scripts', function(){
-    return gulp  
-        .src(['src/js/*.js'])
+    return gulp.src('src/js/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('build/js/'));
+        .pipe(gulp.dest('build/js/'))
+        .pipe(browserSync.stream());
 });
 
 //stylus
 gulp.task('stylus', function(){
-    gulp.src('./src/styl/*.styl')
+    gulp.src('src/styl/*.styl')
         .pipe(stylus({
             use: [nib(), jeet()],
             compress: true
         }))
-        .pipe(gulp.dest('build/css/'));
+        .pipe(gulp.dest('build/css'))
+        .pipe(browserSync.stream());
 });
 
-
-//watch
-gulp.task('watch', function(){
-
-
-    //Scripts
-    gulp.watch('src/js/**/*.js', function(event) {
-        gutil.log('Minificando seu JS ......');
-        gulp.run('scripts');
-    });
-
-    //Compass
-    gulp.watch('src/styl/**/*.styl', function(event) {
-        gutil.log('Compilando o seu Stylus ......');
-        gulp.run('stylus');
-    });
-
-
-});
+//imagens
+// gulp.task('img', function(){
+//     return gulp.src('src/img/*')
+//         .pipe(imagemin({
+//             progressive: true,
+//             use: [pngquant()]
+//         }))
+//         .pipe(gulp.dest('build/img'));
+// })
--- src/js/main.js
+++ src/js/main.js
@@ -1,68 +1,72 @@
-$(window).load(function(){
-	getWeather('New York, NY');
+$(document).ready(function(){
 
-	$('#change').on('click', function(){
-		var newLocation = $('#location').val();
-		getWeather(newLocation);
+	$('input[type="input"]').focus();	
+	
+	var raw_template = $('#weather-template').html();	
+	var template = Handlebars.compile(raw_template);
+	var placeHolder = $('.forecast');
 
-		$('body').fadeOut();
+	weather("New York, NY");
+
+	$('#location-form').submit(function(e){
+		e.preventDefault();
+		$('.loading').fadeIn();	
+		$('.forecast').empty();
+		var location = $('#location').val();
+		weather(location);
+
+		$('.input::before').hide();
 	});
 
-});
-
-function getWeather(theLocation){
-
-	$.ajax({
-	type: 'GET',
-	url: 'https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + theLocation + '")&format=json'
-
-	}).done(function(json){
-
-		$.each(json.query.results.channel.item.forecast, function(i, show){
-
-			console.log(show);
-			$('.forecast').append('<div class="day">' + 'dia: ' + show.day + ' / ' + 'temp: ' + show.high + '</div>');
-
-		});
-
-		var Lat = json.query.results.channel.item.lat;
-		var Long = json.query.results.channel.item.long;
-
-		//console.log('Lat:' + Lat + ', Long:' + Long)
-
-		//Flickr
+	function flickr(lat, long) {
 		$.ajax({
 			type: 'GET',
-			url: 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9ccb20724160d26c4dfb5f4b89963270&lat='+ Lat +'&lon='+ Long +'&group_id=1463451@N25&per_page=1&format=json&nojsoncallback=1'
+			url: 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9ccb20724160d26c4dfb5f4b89963270&lat='+ lat +'&lon='+ long +'&group_id=1463451@N25&per_page=1&format=json&nojsoncallback=1',
+			success: function(data){
+				$.each(data.photos.photo, function(i, photo){
+					var farmId = photo.farm;
+					var serverId = photo.server;
+					var id = photo.id;
+					var secret = photo.secret;
+					var imgUrl = 'https://farm'+ farmId +'.staticflickr.com/'+ serverId +'/'+ id +'_'+ secret +'_b.jpg';
 
-		}).done(function(flickr){
-			
-			$.each(flickr.photos.photo, function(i, photos){
+					$('.mc-image').css('background-image', 'url('+ imgUrl +')');
+					$('.image').css('background-image', 'url('+ imgUrl +')');
+				});
+			}
+		})
+	}
 
-				var farmId = photos.farm;
-				var serverId = photos.server;
-				var id = photos.id;
-				var secret = photos.secret;
-				var imgUrl = 'https://farm'+ farmId +'.staticflickr.com/'+ serverId +'/'+ id +'_'+ secret +'_b.jpg';
+	function weather(location){
+		$.ajax({
+			type: 'GET',
+			url: 'https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '") and u="c" &format=json',
+			success: function(data){
+				console.log(data);
 
-				$('.mc-image').css('background-image', 'url('+ imgUrl +')');
-				$('.image').css('background-image', 'url('+ imgUrl +')');
+				if(data.query.results.channel.item != 'City not found'){
+					$.each(data.query.results.channel.item.forecast, function(index, show){
+						if(index == 0){
+							show.day = 'Today';	
+						}
 
-				$('body').fadeIn();
+						var html = template(show);
+						placeHolder.append(html);
+					});	
+
+					var lat = data.query.results.channel.item.lat;
+					var long = data.query.results.channel.item.long;
+
+					flickr(lat, long);
+				}
 				
-			});
+			},
+			complete: function(){
+				$('.loading').fadeOut();
+			}
+		});	
+	}
 
-		}).fail(function(flickErr){
+	
 
-			console.log(flickErr);	
-
-		});
-
-
-
-	}).fail(function(error){
-
-		console.log(error);
-
-	});
-}
+});
